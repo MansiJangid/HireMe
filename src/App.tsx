96710ef75e3a2ae2f06bc0e46edab81a32bf7ed5
MansiJangid <jangidmansi34@gmailcom>
2025-07-26 00:48:28 +0530

    routing in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom" 
+// import { Button } from './components/ui/button'
 import './index.css'
+import AppLayout from "./layouts/app-layout"
+import LandingPage from "./pages/LandingPage"
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children:[
+      {
+        path:'/',
+        element:<LandingPage />
+      }
+    ]
+  }
+])
 
 function App() {
 
   return (
     <>
-      Hello
+    <RouterProvider router={router}/>
     </>
   )
 }
 
 export default App
+
